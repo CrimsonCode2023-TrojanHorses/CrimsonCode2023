4c4eda0b2c5f1a2dcf6accc8b822cedb23641c9c
Cody Holt <cholt19@outlook.com>
2023-02-19 19:25:12 -0800

    Included saveRecipe()

--- data.py
+++ data.py
@@ -26,8 +26,48 @@ def getRandomRecipe():
             break
         if loop >= count:
             return
+    return data[rnum]    
 
-    return data[rnum]
+def saveRecipe(recipe, key):
+    # Opens json file
+    rf = open("recipe.json", "r+")
+    data = json.load(rf)
+    
+    # Get number of values in list, sets rnum to a number corresponding
+    # to a value in the list
+    count = 0
+    for i in data:
+        count += 1
+    
+    # Detector acts as boolean.
+    # If a recipe with similar key in database:
+    #    Detector = TRUE
+    #        pass
+    # If a recipe with similar key !in database:
+    #    Detector = FALSE
+    #        Add recipe into database
+    detector = 0
+    for i in range(count):
+        temp = data[i]
+        if temp["key"] == key:
+            detector = 1
+        else:
+            pass
+    if detector == 0:
+        recipe["key"] = key
+        data.append(recipe)
+        with open('recipe.json', 'w') as f:
+            json.dump(data, f, indent=4)
+            
+    rf.close()
+
+
+
+
+    
+        
+
+    
 
 
 
